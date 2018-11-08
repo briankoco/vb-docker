a4d56579199a1cdef1160fc009e096ae580b76e6
Brian Kocoloski <brian.kocoloski@wustl.edu>
2018-11-08 12:40:37 -0600

    add docker network creation to setup script

--- setup.sh
+++ setup.sh
@@ -4,7 +4,11 @@ mkdir -p generated-files
 mkdir -p ssh-keys
 
 pushd generated-files
-git clone https://github.com/briankoco/varbench.git
+if [ ! -d varbench ]; then 
+    git clone https://github.com/briankoco/varbench.git
+else
+    git pull
+fi
 popd
 
 pushd ssh-keys
@@ -13,3 +17,5 @@ echo "Host *" > config
 echo "    StrictHostKeyChecking no" >> config
 chmod 644 config
 popd
+
+docker network create --subnet=172.20.0.0/24 vb-net &> /dev/null
